a9bd1a1f8c77a84652d7902ef82cf7660105c14d
ansari7860 <74490452+ansari7860@users.noreply.github.com>
2022-08-09 09:00:26 +0000

    Update home.js

--- home.js
+++ home.js
@@ -8,9 +8,7 @@ const mobile_menu = document.querySelector('#header .nav-bar .nav-list ul');
 const menu_item = document.querySelectorAll('#header .nav-bar .nav-list ul li a');
 
 
-const header = document.querySelector('#
-
-header.container');
+const header = document.querySelector('#header.container');
 
 
 hamburger.addEventListener('click', () => {
